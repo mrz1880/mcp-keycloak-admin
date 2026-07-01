30a8dd88e82e6cd90ea85b2a83ea821851d7966a
Anthony D. <mrz1880@gmail.com>
2026-07-01 09:25:52 +0200

    chore: replace remaining Pandi-Panda example references with neutral names

--- test/domain/policy/realm-access-policy.test.ts
+++ test/domain/policy/realm-access-policy.test.ts
@@ -4,17 +4,17 @@ import { RealmAccessPolicy } from "../../../src/domain/policy/realm-access-polic
 import { RealmName } from "../../../src/domain/shared/realm-name.js";
 
 describe("RealmAccessPolicy", () => {
-  const pandi = RealmName.fromString("demo-realm");
+  const demoRealm = RealmName.fromString("demo-realm");
   const master = RealmName.fromString("master");
 
   it("rejects a realm absent from a non-empty allow-list", () => {
-    const policy = RealmAccessPolicy.of([pandi]);
+    const policy = RealmAccessPolicy.of([demoRealm]);
     expect(() => policy.assertAllowed(master)).toThrow(/master/);
   });
 
   it("allows a realm present in the allow-list", () => {
-    const policy = RealmAccessPolicy.of([pandi]);
-    expect(() => policy.assertAllowed(pandi)).not.toThrow();
+    const policy = RealmAccessPolicy.of([demoRealm]);
+    expect(() => policy.assertAllowed(demoRealm)).not.toThrow();
   });
 
   it("allows any realm when the allow-list is empty", () => {
